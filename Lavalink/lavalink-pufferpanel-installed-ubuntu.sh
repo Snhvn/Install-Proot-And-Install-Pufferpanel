500f3cf8025fb5f108fd028b49e82272ee4e26be
Snhvn <snhvnnguyen@gmail.com>
2025-01-31 09:40:01 +0700

    Update lavalink-pufferpanel-installed-ubuntu.sh

--- Lavalink/lavalink-pufferpanel-installed-ubuntu.sh
+++ Lavalink/lavalink-pufferpanel-installed-ubuntu.sh
@@ -2,7 +2,6 @@
 clear
 echo "Đợi 3s để cài đặt/Wait 3s to install"
 sleep 3
-
 clear
 echo "Loading..... Package/Đang Tải..... Package"
 apt update -y && apt install sudo && apt install wget && apt install curl
@@ -15,11 +14,13 @@ mkdir Lavalink-Puffer
 clear
 echo -e "Nhập phiên bản Lavalink/Enter Lavalink version"
 read versionlavalink
-echo -e "Your version is Lavalink-V3 or Lavalink-V4 choose 1 of 2/Phiên bản của bạn là Lavalink-V3 hoặc Lavalink-V4 chọn 1 trong 2"
+echo -e "Your version is Lavalink-V3 or Lavalink-V4 choose 1 of 2/Phiên bản của bạn là Lavalink-V3 hoặc Lavalink-V4 chọn 1 trong 2.Hãy nhập v3 hoặc v4"
 read v3v4
 cd Lavalink-Puffer
 echo "Bắt đầu tải file Lavalink.jar/Start downloading Lavalink.jar file"
 wget -O Lavalink-$version.jar https://github.com/lavalink-devs/Lavalink/releases/download/$versionlavalink/Lavalink.jar
 echo "Đã Tải Xong/Download completed"
 echo "Tiếp theo tải application.yml/Next download application.yml"
-
+wget -O application.yml https://github.com/Snhvn/Install-Proot-And-Install-Pufferpanel/raw/refs/heads/main/Lavalink/application-$v3v4.yml
+clear
+echo "Đã tải xong file application.yml vui lòng khởi Lavalink hoặc không :)))" 
