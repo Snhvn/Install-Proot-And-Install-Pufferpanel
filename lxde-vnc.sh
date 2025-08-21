37b3e1c0d20a1f681bef38b85d0ca12fb43da958
Snhvn <snhvnnguyen@gmail.com>
2025-08-21 21:57:29 +0700

    Update lxde-vnc.sh

--- lxde-vnc.sh
+++ lxde-vnc.sh
@@ -1,16 +1,35 @@
+#!/bin/bash
 clear
-echo -e "${RED}Downloading... Please Wait"
-    apt update && apt upgrade -y
-    apt install lxde -y
-    apt install xrdp -y
-    echo "lxsession -s LXDE -e LXDE" >> /etc/xrdp/startwm.sh
+
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+
+apt update && apt upgrade -y
+
+
+apt install -y lxde tightvncserver
+
+
+echo -e "${YELLOW}tạo pass vnc cho user hiện tại...${NC}"
+vncpasswd
+echo -e "${YELLOW} Tạo port cho vnc (bắt buộc phải nhập nếu kh bn sẽ bể zái)"
+read port
+vncserver :1
+vncserver -kill :1
+
+
+cat > ~/.vnc/xstartup <<EOF
+#!/bin/bash
+xrdb $HOME/.Xresources
+startlxde &
+EOF
+chmod +x ~/.vnc/xstartup
+
+apt install -y socat
+nohup socat TCP-LISTEN:$port,fork TCP:127.0.0.1:5901 &
+
 clear
-    echo -e "${GREEN}Downloading and installation completed!"
-    echo -e "${YELLOW}Select VNC Port"
-    read selectedPort
- 
-    sed -i "s/port=5900/port=$selectedPort/g" /etc/xrdp/xrdp.ini
-clear
-    service xrdp restart
-    clear
-    echo -e "${GREEN}VNC Created And Started on Port $selectedPort"
+echo -e "${GREEN}VNC server đã được cài đặt và chạy trên port $port.${NC}"
+echo -e "${YELLOW}Kết nối bằng VNC Viewer: 0.0.0.0:$port.${NC}"
