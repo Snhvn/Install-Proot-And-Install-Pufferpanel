453c7d547f77125c9a863f37374aba27094f1a50
Snhvn <snhvnnguyen@gmail.com>
2025-08-22 10:23:36 +0700

    Update lxde-vnc.sh

--- lxde-vnc.sh
+++ lxde-vnc.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 clear
-
+echo "!!Lưu ý: Đây là để chuyển đổi port vnc 5901 sang port được random (ví dụ port: 25678)"
+echo "!!Để giúp làm mượt hơn để treo web hoặc chạy languages j đó bla bla"
+echo "!!Đọc xong lưu ý rồi hãy chờ tầm 3s để bắt đầu cài"
+sleep 3
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
 
 
 apt update && apt upgrade -y
-
-
-apt install -y lxde tightvncserver
-
-
-echo -e "${YELLOW}tạo pass vnc cho user hiện tại...${NC}"
+apt install -y lxde tightvncserver socat
+echo -e "${YELLOW}tạo pass vnc cho user hiện tại...., nhớ đặt mật khẩu cho root r đặt luôn cho pass vnc${NC}"
 vncpasswd
-echo -e "${YELLOW} Tạo port cho vnc (bắt buộc phải nhập nếu kh bn sẽ bể zái)"
+echo -e "${YELLOW} Tạo port cho vnc 5901 (bắt buộc phải nhập nếu kh bn sẽ bể zái)"
+echo "Nhớ đọc ở đầu khi mới bắt đầu vào .sh để chính xác port nếu kh sẽ bị lỗi"
 read port
 vncserver :1
 vncserver -kill :1
@@ -27,9 +27,10 @@ startlxde &
 EOF
 chmod +x ~/.vnc/xstartup
 
-apt install -y socat
+echo "${YELLOW}Đang start port $port cho vnc 5901...."
 nohup socat TCP-LISTEN:$port,fork TCP:127.0.0.1:5901 &
-
+echo "${GREEN}Đã lưu file nohup để chạy nền port ở $(pwd)"
 clear
 echo -e "${GREEN}VNC server đã được cài đặt và chạy trên port $port.${NC}"
 echo -e "${YELLOW}Kết nối bằng VNC Viewer: 0.0.0.0:$port.${NC}"
+vncserver :1
